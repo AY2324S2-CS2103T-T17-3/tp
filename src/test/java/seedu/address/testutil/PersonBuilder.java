98ee269be7cece02ddd7d8cc69d34c037fb9ba54
Chee Jing Jie <jingjie.chee@u.nus.edu>
2024-03-06 21:42:39 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -30,6 +30,9 @@ public class PersonBuilder {
     private Remark remark;
     private Set<Tag> tags;
 
+    /**
+     * All fields are populated with default values.
+     */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
